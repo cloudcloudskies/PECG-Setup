c70a01af8782b6569cc584cee52f45085089d25a
cloudskies <113025487+cloudcloudskies@users.noreply.github.com>
2024-04-09 20:54:09 +0300

    Oops

--- PECG_Setup.bat
+++ PECG_Setup.bat
@@ -30,7 +30,7 @@ echo Installing Lime and OpenFL...
 haxelib run lime setup flixel
 haxelib run openfl setup
 
-echo Done with Lime and OpenFL setup!
+echo Installed Lime and OpenFL.
 
 echo Installing Visual Studio, please wait...
 curl -# -O https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771-41d6-9a1c-fc971e7d03a7/8eb13958dc429a6e6f7e0d6704d43a55f18d02a253608351b6bf6723ffdaf24e/vs_Community.exe
